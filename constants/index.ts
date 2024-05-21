a50e4c19db6c7dc1d1607c496d2b5b34fec9dfc7
Matheus Carmo de Araújo <rerrol2508@gmail.com>
2024-05-21 08:10:29 -0300

    initial code

---
+++ constants/index.ts
@@ -0,0 +1,27 @@
+export const sidebarLinks = [
+  {
+    label: "Inicio",
+    route: "/",
+    imgUrl: "./icons/Home.svg",
+  },
+  {
+    label: "Novidades",
+    route: "/upcoming",
+    imgUrl: "./icons/upcoming.svg",
+  },
+  {
+    label: "Próximos",
+    route: "/previous",
+    imgUrl: "./icons/previous.svg",
+  },
+  {
+    label: "Gravações",
+    route: "/recordings",
+    imgUrl: "./icons/Video.svg",
+  },
+  {
+    label: "Sala Pessoal",
+    route: "/personal-room",
+    imgUrl: "./icons/add-personal.svg",
+  },
+];
